66df4393ddfed59d94d8b662a330e3133e5dd774
Maapeer <matheo.perochon@etu.univ-poitiers.fr>
2022-11-09 13:51:30 +0000

    first version

---
+++ hello.scala
@@ -0,0 +1,21 @@
+object Hello {
+
+    def main(args: Array[String]) =
+{var var3=2
+ 
+	if (var3==1)
+	{
+	println("True")
+	}
+	else
+	{
+	println("False")
+	}
+
+        println("Hello, world")
+
+    }
+
+}
+
+
