d66227d21f4ca41444730203afd22d6f4d10f0d4
Maapeer <matheo.perochon@etu.univ-poitiers.fr>
2022-11-14 16:11:21 +0000

    five version

--- hello.scala
+++ hello.scala
@@ -1,15 +1,16 @@
 object Geeks {
 
+	def multi10(m:Int):Int=m*10
+
     def main(args: Array[String]) 
 {
 	for(a<-1 to args(0).toInt){
 		println("Valeur de a :" +a)
+		multi10(2)
 	
-       
-		}
-
-    }
-
+       }
+	   println(multi10(2))
+	}
 }
 
 
