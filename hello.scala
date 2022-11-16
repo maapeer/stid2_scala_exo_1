97069a0bba70ec4a4d376c62fb740fb3a7dde6be
Maapeer <matheo.perochon@etu.univ-poitiers.fr>
2022-11-16 09:26:29 +0000

    eight version

--- hello.scala
+++ hello.scala
@@ -9,9 +9,14 @@ object Geeks {
 			var chiffres:List[Int]=List(1,3,9,7)
 			for(element<-chiffres) println(element);
 
-		val listeDelistes : List[List[Int]]=List(List(3,2),List(2,5,7))
-		val evens = chiffres.filter(_<=3)
-		println(evens)
+			val listeDelistes : List[List[Int]]=List(List(3,2),List(2,5,7))
+
+			var chiffres2:List[Int]=List(2,5,6,10)
+			val concat = List.concat(chiffres,chiffres2)
+			println(concat)
+			
+			val evens = chiffres.filter(_<=3)
+			println(evens)
 	}
 
 			
