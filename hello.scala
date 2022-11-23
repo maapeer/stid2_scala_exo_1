8f1ab6eced01185d728ceed9fe590edcf4d2a8c7
Maapeer <matheo.perochon@etu.univ-poitiers.fr>
2022-11-23 15:13:42 +0000

    exo3

--- hello.scala
+++ hello.scala
@@ -3,20 +3,24 @@ object Geeks {
 
 	def main(args:Array[String])
 	{ 
-			val fruits:List[String]=List("Bananes","oranges","pommes")
-			fruits.foreach(println);
+		
+			val lettres:List[String]=List("AA","BBB","C","DDDD")
+			lettres.map(ele=>(ele,ele.length)).foreach(println)
 
-			var chiffres:List[Int]=List(1,3,9,7)
-			for(element<-chiffres) println(element);
 
-			val listeDelistes : List[List[Int]]=List(List(3,2),List(2,5,7))
 
-			var chiffres2:List[Int]=List(2,5,6,10)
-			val concat = List.concat(chiffres,chiffres2)
-			println(concat)
+
+			//var chiffres:List[Int]=List(1,3,9,7)
+			//for(element<-chiffres) println(element);
+
+			//val listeDelistes : List[List[Int]]=List(List(3,2),List(2,5,7))
+
+			//var chiffres2:List[Int]=List(2,5,6,10)
+			//val concat = List.concat(chiffres,chiffres2)
+			//println(concat)
 			
-			val evens = chiffres.filter(_<=3)
-			println(evens)
+			//val evens = chiffres.filter(_<=3)
+			//println(evens)
 	}
 
 			
