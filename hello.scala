cc8a97b9e093b352423a620b1593377f1ef4d8ce
Maapeer <matheo.perochon@etu.univ-poitiers.fr>
2022-11-23 15:57:06 +0000

    exo4

--- hello.scala
+++ hello.scala
@@ -4,9 +4,17 @@ object Geeks {
 	def main(args:Array[String])
 	{ 
 		
-			val lettres:List[String]=List("AA","BBB","C","DDDD")
-			lettres.map(ele=>(ele,ele.length)).foreach(println)
+			//val lettres:List[String]=List("AA","BBB","C","DDDD")
+			//lettres.map(ele=>(ele,ele.length)).foreach(println)
 
+			val num=List(1,1,2,2,3,4,3,5)
+			//num.groupBy(identity).mapValues(ele=>(ele.length)).foreach(println)
+			num.groupBy (x=>x).map(x=>(x._1,x._2.sum)).foreach(println)
+			//println(num.groupBy (x=>x))
+			
+
+
+			
 
 
 
